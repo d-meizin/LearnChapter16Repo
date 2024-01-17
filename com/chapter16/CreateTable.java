123ea1f320e15931b246f8989b736a20a0c154dd
d-meizin <d.meizin@gmail.com>
2024-01-17 16:04:48 +0300

    added class CreateTable

---
+++ com/chapter16/CreateTable.java
@@ -0,0 +1,15 @@
+package com.chapter16;
+
+import java.sql.DriverManager;
+import java.sql.Statement;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+class CreateTable {
+	
+	public static void main(String[] args) {
+		final String DRIVER = "org.apache.derby.jdbc.EmbeddedDriver";
+		
+	}
+
+}
